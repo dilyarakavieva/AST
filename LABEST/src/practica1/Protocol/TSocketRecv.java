c2d36a49eaeca559939bd2c73c3eb523bde05e6e
dilyarakavieva <diliara.kavieva@estudiantat.upc.edu>
2024-10-03 12:45:33 +0200

    Update TSocketRecv.java

--- LABEST/src/practica1/Protocol/TSocketRecv.java
+++ LABEST/src/practica1/Protocol/TSocketRecv.java
@@ -6,12 +6,21 @@ import util.SimNet;
 
 public class TSocketRecv extends TSocket_base {
 
-  public TSocketRecv(SimNet network) {
-    super(network);
-  }
+    public TSocketRecv(SimNet network) {
+        super(network);
+    }
 
-  @Override
-  public int receiveData(byte[] data, int offset, int length) {
-    throw new RuntimeException("//Completar...");
-  }
+    @Override
+    public int receiveData(byte[] data, int offset, int length) {
+        int num;
+        TCPSegment seg = this.network.receive();
+        byte[] dataS = seg.getData();
+
+        num = Math.min(dataS.length, length);
+        System.arraycopy(dataS, 0, data, offset, length);
+
+        printRcvSeg(seg);
+
+        return num;
+    }
 }
