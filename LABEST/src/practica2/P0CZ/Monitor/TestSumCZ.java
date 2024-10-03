6a1dfe70fed512b80c0c7be7246e05b0792b1156
dilyarakavieva <diliara.kavieva@estudiantat.upc.edu>
2024-10-03 12:24:54 +0200

    Update TestSumCZ.java

--- LABEST/src/practica2/P0CZ/Monitor/TestSumCZ.java
+++ LABEST/src/practica2/P0CZ/Monitor/TestSumCZ.java
@@ -3,6 +3,13 @@ package practica2.P0CZ.Monitor;
 public class TestSumCZ {
 
     public static void main(String[] args) throws InterruptedException {
-        throw new RuntimeException("//Completar...");
+        MonitorCZ mon = new MonitorCZ();
+        CounterThreadCZ f1 = new CounterThreadCZ(mon);
+        CounterThreadCZ f2 = new CounterThreadCZ(mon);
+       f1.run();
+       f2.run();
+        System.out.println(mon.getX());
+       
+        
     }
 }
