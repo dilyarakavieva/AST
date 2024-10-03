7b375b799b28f803e5cabadf0649f8fe98ab75d6
dilyarakavieva <diliara.kavieva@estudiantat.upc.edu>
2024-10-03 12:22:57 +0200

    Update MonitorSync.java

--- LABEST/src/practica2/P1Sync/Monitor/MonitorSync.java
+++ LABEST/src/practica2/P1Sync/Monitor/MonitorSync.java
@@ -1,23 +1,48 @@
 package practica2.P1Sync.Monitor;
 
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MonitorSync {
 
     private final int N;
-    
-    //Completar...
+    private final Lock l;
+    boolean pasat;
+    private final Condition entra;
+    private int ultim;
 
     public MonitorSync(int N) {
         this.N = N;
+        l = new ReentrantLock();
+        pasat = false;
+        entra = l.newCondition();
+
     }
 
     public void waitForTurn(int id) {
-        throw new RuntimeException("//Completar...");
+        l.lock();
+        try {
+            while (ultim == id) {
+                entra.awaitUninterruptibly();
+            }
+            ultim = id;
+            pasat = true;
+        } finally {
+            l.unlock();
+        }
+
     }
 
     public void transferTurn() {
-        throw new RuntimeException("//Completar...");
+        l.lock();
+        try {
+            if (pasat) {
+                entra.signal();
+            }
+        } finally {
+            l.unlock();
+        }
+
     }
 }
