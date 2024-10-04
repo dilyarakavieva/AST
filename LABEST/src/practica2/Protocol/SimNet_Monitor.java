7297931977b146e58abb3fa3ebfe47c7134ead87
dilyarakavieva <diliara.kavieva@estudiantat.upc.edu>
2024-10-04 09:48:22 +0200

    Update SimNet_Monitor.java

--- LABEST/src/practica2/Protocol/SimNet_Monitor.java
+++ LABEST/src/practica2/Protocol/SimNet_Monitor.java
@@ -1,6 +1,7 @@
 package practica2.Protocol;
 
 import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import practica1.CircularQ.CircularQueue;
 import util.Const;
@@ -9,27 +10,51 @@ import util.SimNet;
 
 public class SimNet_Monitor implements SimNet {
 
-  protected CircularQueue<TCPSegment> queue;
-  //Completar
+    protected CircularQueue<TCPSegment> queue;
+    protected Lock l;
+    protected Condition r,s;
 
-  public SimNet_Monitor() {
-    queue  = new CircularQueue<>(Const.SIMNET_QUEUE_SIZE);
-    //Completar
-  }
+    public SimNet_Monitor() {
+        queue = new CircularQueue<>(Const.SIMNET_QUEUE_SIZE);
+        l = new ReentrantLock();
+        r = l.newCondition();
+        s = l.newCondition();
+    
+    }
 
-  @Override
-  public void send(TCPSegment seg) {
-    throw new RuntimeException("//Completar...");
-  }
+    @Override
+    public void send(TCPSegment seg) {
+        l.lock();
+        try {
+            while (queue.full()) {
+                s.awaitUninterruptibly();
+            }
+            queue.put(seg);
+            r.signal();
+        } finally {
+            l.unlock();
+        }
+    }
 
-  @Override
-  public TCPSegment receive() {
-    throw new RuntimeException("//Completar...");
-  }
+    @Override
+    public TCPSegment receive() {
+        l.lock();
+        TCPSegment seg= new TCPSegment();
+        try {
+            while (queue.empty()) {
+                r.awaitUninterruptibly();
+            }
+            seg = queue.get();
+            s.signal();
+        } finally {
+            l.unlock();
+        }
+        return seg;
+    }
 
-  @Override
-  public int getMTU() {
-    throw new UnsupportedOperationException("Not supported yet. NO cal completar fins a la pràctica 3...");
-  }
+    @Override
+    public int getMTU() {
+        throw new UnsupportedOperationException("Not supported yet. NO cal completar fins a la pràctica 3...");
+    }
 
 }
