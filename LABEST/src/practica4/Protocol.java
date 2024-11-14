543948d67f7385aa86e4594c0c7b3c93c82e66d6
dilyarakavieva <diliara.kavieva@estudiantat.upc.edu>
2024-11-14 10:30:01 +0100

    Update Protocol.java

--- LABEST/src/practica4/Protocol.java
+++ LABEST/src/practica4/Protocol.java
@@ -8,17 +8,33 @@ import util.TSocket_base;
 public class Protocol extends Protocol_base {
 
     public Protocol(SimNet network) {
-      super(network);
+        super(network);
     }
 
     protected void ipInput(TCPSegment seg) {
-        throw new RuntimeException("//Completar...");
+        TSocket_base nou = this.getMatchingTSocket(seg.getSourcePort(), seg.getDestinationPort());
+        if (nou != null) {
+            
+            //seg.setDestinationPort(nou.remotePort);// se puede cambiar puerta de seg q llega?
+            //seg.setSourcePort(nou.localPort);
+            nou.processReceivedSegment(seg);
+        } else {
+            log.printPURPLE("no tenemos TSocket_base para este segmento!");
+        }
     }
 
     protected TSocket_base getMatchingTSocket(int localPort, int remotePort) {
         lk.lock();
         try {
-            throw new RuntimeException("//Completar...");
+            TSocket_base base = new TSocket_base(network);
+            for (TSocket_base nou : this.activeSockets) {
+                if (nou.localPort == remotePort && nou.remotePort == localPort) {
+                    base = nou;
+                    
+                }
+
+            }
+            return base;
         } finally {
             lk.unlock();
         }
